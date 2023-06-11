0538a26811523033b98f22629613ba6c3ca5c759
zajac-paulinaa <bye.zajac@gmail.com>
2023-06-11 12:05:23 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  const subject = props.subject;
+  return (
+    <div className="App">
+       <header className="App-header">
+       <img src={logo} className="App-logo" alt="logo" />
+      <p>Hello, {subject}!</p>
+    </header>
+   </div>
+ );
+}
+
+export default App;
